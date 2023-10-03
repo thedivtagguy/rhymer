c99bdc22cea1b801c7e7e9256f2928729a991a52
thedivtagguy <amanbhargava2001@gmail.com>
2023-10-04 01:54:27 +0530

    fix: searchParams

--- src/routes/room/+page.svelte
+++ src/routes/room/+page.svelte
@@ -1,9 +1,14 @@
 <script>
 	import GameLogic from '../../components/GameLogic.svelte';
 	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
 
-	let roomId = $page.url.searchParams.get('id');
-	console.log(roomId);
+	let roomId;
+
+	onMount(() => {
+		// save the id parameter if it's in the url or an empty string
+		roomId = $page.url.searchParams.get('id') || '';
+	});
 </script>
 
 <GameLogic id={roomId} />
