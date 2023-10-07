28a3c1ef749dcc07a60532537a1138ed2ecd83c9
thedivtagguy <amanbhargava2001@gmail.com>
2023-10-07 08:44:20 +0530

    feat: Hexagon markers

--- src/lib/svg/GuessMarker.svelte
+++ src/lib/svg/GuessMarker.svelte
@@ -13,17 +13,18 @@
 	let cy = radius;
 
 	$: effectiveFill = playerId === userId || gameFinished ? fill : defaultFill;
-
 	$: effectiveStroke = playerId === userId || gameFinished ? stroke : defaultStroke;
+
+	$: points = `
+		${cx},${cy - radius}
+		${cx + (Math.sqrt(3) / 2) * radius},${cy - radius / 2}
+		${cx + (Math.sqrt(3) / 2) * radius},${cy + radius / 2}
+		${cx},${cy + radius}
+		${cx - (Math.sqrt(3) / 2) * radius},${cy + radius / 2}
+		${cx - (Math.sqrt(3) / 2) * radius},${cy - radius / 2}
+	`;
 </script>
 
-<svg height={radius * 2} width={radius * 2}>
-	<circle
-		{cx}
-		{cy}
-		r={radius}
-		fill={effectiveFill}
-		stroke={effectiveStroke}
-		stroke-width={strokeWidth}
-	/>
+<svg height={radius * 2} width={radius * 2} transform="rotate(90deg)">
+	<polygon {points} fill={effectiveFill} stroke={effectiveStroke} stroke-width={strokeWidth} />
 </svg>
